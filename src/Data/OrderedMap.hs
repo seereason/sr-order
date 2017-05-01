07da5e7b7dc5d24ea557cd54c169eb219299dffd
David Fox <dsf@seereason.com>
2017-04-30 18:16:26 -0700

    * Version 0.41 * Safe and unsafe versions of all functions * Safe functions use a new error type

--- src/Data/OrderedMap.hs
+++ src/Data/OrderedMap.hs
@@ -6,12 +6,15 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
 {-# OPTIONS -Wall #-}
 
 module Data.OrderedMap
     ( OrderedMap(..)
+    , OrderError(..)
     , putItem
     , viewByKey
     , showOrder
@@ -29,33 +32,13 @@ import Data.List as List (elem, filter, notElem, partition)
 import Data.Map as Map (Map)
 import qualified Data.Map as Map
 
+-- | Minimum implementation: OKey, OValue
 class (Eq (OKey o), Ord (OKey o), Enum (OKey o)) => OrderedMap o where
     type OKey o
     type OValue o
 
-    empty :: o
-    nextKey :: o -> OKey o
-    newKey :: o -> (OKey o, o)
-    fromPairs :: [(OKey o, OValue o)] -> o
-    toPairs :: o -> [(OKey o, OValue o)]
-    fromMapAndList :: Map (OKey o) (OValue o) -> [OKey o] -> o
-    fromMapListKey :: Map (OKey o) (OValue o) -> [OKey o] -> OKey o -> o
-    toMap :: o -> Map (OKey o) (OValue o)
-    -- toMap = Map.fromList . toPairs
-    toKeys :: o -> [OKey o]
-    -- toKeys = map fst . toPairs
-
     size :: o -> Int
     size = Map.size . toMap
-
-    -- | Unsafe permute function - does not check whether first argument
-    -- is a permutation of the existing keys.
-    permuteUnsafe :: [OKey o] -> o -> o
-    permuteUnsafe keys o = fromMapListKey (toMap o) keys (nextKey o)
-
-    -- | Set the value associated with a key
-    alter :: (Maybe (OValue o) -> Maybe (OValue o)) -> OKey o -> o -> o
-    alter f k o = fromMapListKey (Map.alter f k (toMap o)) (toKeys o) (nextKey o)
 
     lookByKey :: OKey o -> o -> Maybe (OValue o)
     lookByKey k o = Map.lookup k (toMap o)
@@ -63,47 +46,122 @@ class (Eq (OKey o), Ord (OKey o), Enum (OKey o)) => OrderedMap o where
     lookByPos pos o = case drop pos (toKeys o) of
                           k : _ -> lookByKey k o
                           _ -> Nothing
-    deleteByKey :: OKey o -> o -> o
-    deleteByKey k o =
+
+    nextKey :: o -> OKey o
+    newKey :: o -> (OKey o, o)
+
+    empty :: o
+    fromPairs :: [(OKey o, OValue o)] -> o
+    -- | (unsafe)
+    fromMapAndList :: Map (OKey o) (OValue o) -> [OKey o] -> o
+    -- | (unsafe)
+    fromMapListKey :: Map (OKey o) (OValue o) -> [OKey o] -> OKey o -> o
+
+    toPairs :: o -> [(OKey o, OValue o)]
+    toMap :: o -> Map (OKey o) (OValue o)
+    -- toMap = Map.fromList . toPairs
+    toKeys :: o -> [OKey o]
+    -- toKeys = map fst . toPairs
+
+    -- | Set the value associated with a key.  If the key is not
+    -- present a new entry is created (where?)
+    alter :: (Maybe (OValue o) -> Maybe (OValue o)) -> OKey o -> o -> o
+    alter f k o = fromMapListKey (Map.alter f k (toMap o)) (toKeys o) (nextKey o)
+
+    -- | Unsafe permute function - does not check whether first argument
+    -- is a permutation of the existing keys.
+    permuteUnsafe :: [OKey o] -> o -> o
+    permuteUnsafe keys o = fromMapListKey (toMap o) keys (nextKey o)
+
+    -- | Move the head element to a specified position.
+    moveHeadUnsafe :: Int -> o -> o
+    moveHeadUnsafe 0 o = o
+    moveHeadUnsafe n o =
+        case toKeys o of
+          [] -> o
+          (k : ks) ->
+              let (ks1, ks2) = splitAt n ks in
+              permuteUnsafe (ks1 ++ [k] ++ ks2) o
+
+    moveHead :: Int -> o -> Either (OrderError o) o
+    moveHead _ o | size o == 0 = Left EmptyOrder
+    moveHead n o | n > size o = Left (OutOfRange n o)
+    moveHead n o = Right (moveHeadUnsafe n o)
+
+    -- | Delete if present
+    deleteByKeyMaybe :: OKey o -> o -> o
+    deleteByKeyMaybe k o =
         fromMapListKey (Map.delete k (toMap o)) (filter (/= k) (toKeys o)) (nextKey o)
-    deleteByPos :: Int -> o -> o
-    deleteByPos pos o = case drop pos (toKeys o) of
-                          k : _ -> deleteByKey k o
-                          _ -> o
+    deleteByKey :: OKey o -> o -> Either (OrderError o) o
+    deleteByKey k o =
+        maybe (Left (InvalidKey k o))
+              (\_ -> Right (deleteByKeyMaybe k o))
+              (Map.lookup k (toMap o))
+
+    deleteByPosUnsafe :: Int -> o -> o
+    deleteByPosUnsafe n o =
+        case drop n (toKeys o) of
+          k : _ -> deleteByKeyMaybe k o
+          _ -> o
+    deleteByPos :: Int -> o -> Either (OrderError o) o
+    deleteByPos n o | n > size o = Left (OutOfRange n o)
+    deleteByPos n o = Right (deleteByPosUnsafe n o)
 
     -- | Put a new element at the beginning of the order, returning a
     -- pair containing the new OrderedMap and the new key.
     prepend :: OValue o -> o -> (o, OKey o)
-    prepend v o = let (k, o') = newKey o in (prependWithKey k v o', k)
+    prepend v o = let (k, o') = newKey o in (prependWithKeyUnsafe k v o', k)
 
-    prependWithKey :: OKey o -> OValue o -> o -> o
-    prependWithKey k _ o | Map.member k (toMap o) = error "prependWithKey"
-    prependWithKey k v o =
+    prependWithKey :: OKey o -> OValue o -> o -> Either (OrderError o) o
+    prependWithKey k _ o | Map.member k (toMap o) = Left (DuplicateKey k o)
+    prependWithKey k v o = Right (prependWithKeyUnsafe k v o)
+
+    prependWithKeyUnsafe :: OKey o -> OValue o -> o -> o
+    prependWithKeyUnsafe k _ o | Map.member k (toMap o) = error "prependWithKey"
+    prependWithKeyUnsafe k v o =
         fromMapListKey (Map.insert k v (toMap o)) (k : toKeys o) (max (succ k) (nextKey o))
 
-    -- | Move the head element to a specified position.
-    moveHead :: Int -> o -> o
-    moveHead 0 o = o
-    moveHead n o =
-        case toKeys o of
-          [] -> o
-          (k : ks) -> let (ks1, ks2) = splitAt n ks in
-                      permuteUnsafe (ks1 ++ [k] ++ ks2) o
-
     -- | Insert an element at a specific position.
-    insertAt :: {-OrderedMap o =>-} Int -> OValue o -> o -> (o, OKey o)
-    insertAt n v o = let (o', k) = prepend v o in (moveHead n o', k)
+    insertAtUnsafe :: Int -> OValue o -> o -> (o, OKey o)
+    insertAtUnsafe n v o = let (o', k) = prepend v o in (moveHeadUnsafe n o', k)
 
-    insertWithKey :: {-0OrderedMap o =>-} Int -> OKey o -> OValue o -> o -> o
-    insertWithKey n k v o = let o' = prependWithKey k v o in moveHead n o'
+    insertAt :: Int -> OValue o -> o -> Either (OrderError o) (o, OKey o)
+    insertAt n _ o | n > size o = Left (OutOfRange n o)
+    insertAt n v o = Right (insertAtUnsafe n v o)
+
+    insertWithKeyUnsafe :: Int -> OKey o -> OValue o -> o -> o
+    insertWithKeyUnsafe n k v o = let o' = prependWithKeyUnsafe k v o in moveHeadUnsafe n o'
+
+    insertWithKey :: Int -> OKey o -> OValue o -> o -> Either (OrderError o) o
+    insertWithKey n _ _ o | n > size o = Left (OutOfRange n o)
+    insertWithKey _ k _ o | Map.member k (toMap o) = Left (DuplicateKey k o)
+    insertWithKey n k v o = let o' = prependWithKeyUnsafe k v o in Right (moveHeadUnsafe n o')
 
     -- | Put a new element at the end of the OrderedMap, returning a pair
     -- containing the new OrderedMap and the new key.
     append :: OValue o -> o -> (o, OKey o)
-    append v o = insertAt (size o) v o
+    append v o = insertAtUnsafe (size o) v o
 
-    appendWithKey :: OKey o -> OValue o -> o -> o
-    appendWithKey k v o = insertWithKey (size o) k v o
+    appendWithKeyUnsafe :: OKey o -> OValue o -> o -> o
+    appendWithKeyUnsafe k v o = insertWithKeyUnsafe (size o) k v o
+
+    appendWithKey :: OKey o -> OValue o -> o -> Either (OrderError o) o
+    appendWithKey k _ o | Map.member k (toMap o) = Left (DuplicateKey k o)
+    appendWithKey k v o = Right (appendWithKeyUnsafe k v o)
+
+data OrderError o
+    = InvalidKey (OKey o) o
+    | InvalidPermutation o [(OKey o, OValue o)] [OKey o]
+    -- ^ Description of problems with an attempt to permute a list.
+    -- Missing pairs are those not mentioned in the new list, invalid
+    -- keys are those mentioned but not present in the old OrderedMap.
+    | DuplicateKey (OKey o) o
+    | OutOfRange Int o
+    | EmptyOrder -- ^ Expected an order with at least one element
+
+deriving instance (Eq o, Eq (OKey o), Eq (OValue o)) => Eq (OrderError o)
+deriving instance (Ord o, Ord (OKey o), Ord (OValue o)) => Ord (OrderError o)
+deriving instance (Show o, Show (OKey o), Show (OValue o)) => Show (OrderError o)
 
 -- | Update the value of an existing item
 putItem :: OrderedMap o => OKey o -> OValue o -> o -> o
@@ -116,17 +174,15 @@ putItem k a m = alter f k m
 viewByKey :: forall o. OrderedMap o => OKey o -> o -> Maybe (OValue o, o) -- like Data.Set.minView
 viewByKey k o =
     case lookByKey k o of
-      Just v -> Just (v, deleteByKey k o)
+      Just v -> Just (v, deleteByKeyMaybe k o)
       Nothing -> Nothing
 
 showOrder :: (OrderedMap o, Show (OValue o), Show (OKey o)) => o -> String
 showOrder o = "(fromPairs (" ++ show (toPairs o) ++ "))"
 
--- | Replace the current ordering with the given key list.  The
--- result is a triple: (new, missing, invalid).  Missing pairs are
--- those not mentioned in the new list, invalid are those
--- mentioned but not present in the old OrderedMap.
-permute :: OrderedMap o => [OKey o] -> o -> (o, [(OKey o, OValue o)], [OKey o])
+-- | Replace the current ordering with the given key list, which must
+-- be the same length and contain the same keys as the order.
+permute :: OrderedMap o => [OKey o] -> o -> Either (OrderError o) o
 permute neworder m =
     reorder $ collect $ sanitize
     where
@@ -143,9 +199,9 @@ permute neworder m =
       -- reorder :: ([k], [k], [k]) -> (OrderMap k a, OrderMap k a, [k])
       reorder (valid, _missing, invalid) =
           let (validmap, missingmap) = Map.partitionWithKey (\ k _ -> List.elem k valid) (toMap m) in
-          (fromMapAndList validmap valid,
-           (Map.toList missingmap),
-           invalid)
+          if Map.null missingmap && null invalid
+          then Right (fromMapAndList validmap valid)
+          else Left (InvalidPermutation m (Map.toList missingmap) invalid)
 
 insertItems :: OrderedMap o => o -> [OValue o] -> ([OKey o], o)
 insertItems om xs =
--- tests/Main.hs
+++ tests/Main.hs
@@ -25,25 +25,38 @@ prop_delete :: Order Int String -> Property
 prop_delete o | OrderedMap.size o == 0 = property True
 prop_delete o =
     forAll (choose (0, OrderedMap.size o - 1)) $ \pos ->
-    OrderedMap.size (deleteByPos pos o) == OrderedMap.size o - 1
+    fmap OrderedMap.size (deleteByPos pos o) == Right (OrderedMap.size o - 1)
 
 prop_insertAt :: String -> Order Int String -> Property
 prop_insertAt v o =
     forAll (choose (0, OrderedMap.size o)) $ \pos ->
-    OrderedMap.size (fst (insertAt pos v o)) == OrderedMap.size o + 1
+    fmap (OrderedMap.size . fst) (insertAt pos v o) == Right (OrderedMap.size o + 1)
 
 -- | Use an explicit generator to create a valid list position.
 prop_insert_delete :: String -> Order Int String -> Property
 prop_insert_delete v o =
     forAll (choose (0, OrderedMap.size o)) $ \pos ->
-        (let (o', k) = insertAt pos v o in deleteByKey k o') == o
+        (let Right (o', k) = insertAt pos v o in deleteByKey k o') == Right o
 
 prop_insert_delete_pos :: String -> Order Int String -> Property
 prop_insert_delete_pos v o =
     forAll (choose (0, OrderedMap.size o)) $ \pos ->
-        (let (o', _) = insertAt pos v o in deleteByPos pos o') == o
+        (let Right (o', _) = insertAt pos v o in deleteByPos pos o') == Right o
+
+newtype AlterTestCase a = AlterTestCase a deriving Show
+
+instance Arbitrary (AlterTestCase (Order Int String)) where
+    arbitrary = elements (fmap AlterTestCase [ fromPairs [(1,"1"), (2, "2"), (3, "3"), (4, "4")]
+                                             -- , fromPairs [(1,"1"), (2, "2"), (4, "4")]
+                                             -- , fromPairs []
+                                             ])
+
+prop_alter_test :: AlterTestCase (Order Int String) -> Bool
+prop_alter_test (AlterTestCase o) =
+    OrderedMap.alter (maybe (Just "new") (\_ -> Just "new")) 3 o == fromMapListKey (Map.fromList [(1,"1"),(2,"2"),(3,"new"),(4,"4")]) ([1,2,3,4]) (5)
 
 return []
+tests :: IO Bool
 tests = $quickCheckAll
 
 main :: IO ()
