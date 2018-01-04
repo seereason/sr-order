df26ff0f2a5482d9cbbed5e7e29d524cb60858af
Jeremy Shaw <jeremy@n-heptane.com>
2018-01-04 15:30:19 -0600

    fix for template-haskell 2.12

--- src/Data/Order.hs
+++ src/Data/Order.hs
@@ -163,7 +163,9 @@ deriveOrder ityp t supers = do
   let idname = mkName (nameBase t ++ "ID")
       unname = mkName ("un" ++ nameBase t ++ "ID")
       mpname = mkName (nameBase t ++ "s")
-#if MIN_VERSION_template_haskell(2,11,0)
+#if MIN_VERSION_template_haskell(2,12,0)
+  idtype <- newtypeD (cxt []) idname [] Nothing (recC idname [varStrictType unname (strictType notStrict ityp) ]) [derivClause Nothing (map conT $ [''Eq, ''Ord, ''Read, ''Show, ''Data, ''Typeable] ++ supers)]
+#elif MIN_VERSION_template_haskell(2,11,0)
   idtype <- newtypeD (cxt []) idname [] Nothing (recC idname [varStrictType unname (strictType notStrict ityp) ]) (sequence $ map conT $ [''Eq, ''Ord, ''Read, ''Show, ''Data, ''Typeable] ++ supers)
 #else
   idtype <- newtypeD (cxt []) idname [] (recC idname [varStrictType unname (strictType notStrict ityp) ]) ([''Eq, ''Ord, ''Read, ''Show, ''Data, ''Typeable] ++ supers)
