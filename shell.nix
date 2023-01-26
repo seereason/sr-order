6e5ad6da34c8248e57462fdc1cb4d75d61171dc0
David Fox <dsf@seereason.com>
2023-01-26 06:24:57 -0800

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -5,18 +5,21 @@ let
   inherit (nixpkgs) pkgs;
 
   f = { mkDerivation, assoc-listlike, base, cereal, containers
-      , data-default, extra, HUnit, lens, ListLike, pretty, QuickCheck
-      , safecopy, sr-extra, stdenv, vector
+      , data-default, extra, generic-lens, HUnit, lens, ListLike, mtl
+      , pretty, QuickCheck, safecopy, sr-utils, stdenv, vector
       }:
       mkDerivation {
         pname = "sr-order";
-        version = "1.7";
+        version = "1.11.1";
         src = ./.;
         libraryHaskellDepends = [
-          assoc-listlike base cereal containers data-default lens ListLike
-          pretty QuickCheck safecopy sr-extra vector
+          assoc-listlike base cereal containers data-default extra
+          generic-lens lens ListLike mtl pretty QuickCheck safecopy sr-utils
+          vector
         ];
-        testHaskellDepends = [ base containers extra HUnit QuickCheck ];
+        testHaskellDepends = [
+          base containers extra HUnit lens mtl QuickCheck
+        ];
         homepage = "https://github.com/seereason/th-path";
         description = "A data structure to support lists in collaborative software";
         license = stdenv.lib.licenses.bsd3;
