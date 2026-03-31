42db562db23342327e9bd1ae7c169581c0d53677
Clifford Beshers <beshers@seereason.com>
2026-03-31 14:15:06 -0700

    simpler flake connected to libs

--- flake.nix
+++ flake.nix
@@ -1,34 +1,7 @@
 {
-  outputs = { ... }: {};
+  inputs.sr-libs.url = "git+ssh://git@github.com/seereason/sr-flake?dir=sr-libs&ref=main";
+
+  outputs = { self, sr-libs }: {
+    devShells = sr-libs.devShells;
+  };
 }
-# {
-#   description = "lens-path";
-
-#   inputs = {
-#     pkgsets.url   = "git+ssh://git@github.com/cliffordbeshers/cbgit?dir=nix-config/sr-flake/sr-nixpkgs&ref=main";
-#     sr-libs.url   = "git+ssh://git@github.com/cliffordbeshers/cbgit?dir=nix-config/sr-flake/sr-libs&ref=main";
-#     flake-utils.url = "github:numtide/flake-utils";
-#   };
-
-#   outputs = { self, pkgsets, sr-libs, flake-utils }:
-#     flake-utils.lib.eachDefaultSystem (system:
-#       let
-#         pkgs        = pkgsets.lib.${system}.pkgs;
-#         haskellPkgs = pkgsets.lib.${system}.ghcPackages.extend (sr-libs.overlay pkgs);
-
-#         lens-path = haskellPkgs.callCabal2nix "lens-path" ./. {};
-#       in {
-#         packages.default = lens-path;
-
-#         devShells.default = haskellPkgs.shellFor {
-#           packages    = p: [ p.lens-path ];
-#           buildInputs = [
-#             haskellPkgs.cabal-install
-#             haskellPkgs.haskell-language-server
-#             haskellPkgs.hlint
-#             pkgs.cabal2nix
-#           ];
-#           withHoogle = true;
-#         };
-#       });
-# }
