ebfe8735fb9db987eecef3e38851993fc42de943
Clifford Beshers <beshers@seereason.com>
2026-04-01 13:40:33 -0700

    revert to id flake

--- flake.nix
+++ flake.nix
@@ -1,7 +1,3 @@
 {
-  inputs.sr-libs.url = "git+ssh://git@github.com/seereason/sr-flake?dir=sr-libs&ref=main";
-
-  outputs = { self, sr-libs }: {
-    devShells = sr-libs.devShells;
-  };
+  outputs = { ... }: {};
 }
